a82ff808f84d09f0966f8b0a6bcf3fdce6bab08c
son loku <jacobrborgen@gmail.com>
2020-03-07 22:38:01 -0600

    added function to update roles for employees

--- javascript/server.js
+++ javascript/server.js
@@ -24,6 +24,7 @@ connection.connect(function(err) {
   start();
 });
 
+//function starting question cycle
 function start() {
     //initial question
 
@@ -44,13 +45,15 @@ function start() {
             console.log ("view all employees!!!");
             viewAllEmployees();
         }
-        //if view all departments is chosen
-        else if (answers.firstQuestion === "View All Departments") {
-            console.log ("view all departments here!")
+        //if view all employees by departments is chosen
+        else if (answers.firstQuestion === "View All Employees by Departments") {
+            console.log ("view all employees by departments here!")
+            viewByDepartment();
         }
-         //if view all roles is chosen
-         else if (answers.firstQuestion === "View All Roles") {
-            console.log ("view all roles here!")
+         //if view all employees by roles is chosen
+         else if (answers.firstQuestion === "View All Employees by Role") {
+            console.log ("view all employees by roles here!")
+            viewByRole();
         }
          //if add employee is chosen
          else if (answers.firstQuestion === "Add Employee") {
@@ -70,6 +73,7 @@ function start() {
          //if update employee role is chosen
          else if (answers.firstQuestion === "Update Employee Role") {
             console.log ("update roles here!")
+            updateEmployeeRole()
         }
     });
 };
@@ -129,7 +133,7 @@ function createDepartment() {
             choices: function() {
               
                 var choice = results.map(({ id, name }) => ({
-                    value: id, name: `${id} ${name}`
+                    value: id, name: `${name}`
                   }));
                   console.log(choice);
                   return choice;
@@ -184,8 +188,8 @@ function createDepartment() {
                 message: "What is this employee's role?",
                 choices: function() {
               
-                    var choice = results.map(({ id, name }) => ({
-                        value: id, name: `${id} ${name}`
+                    var choice = results.map(({ id, title }) => ({
+                        value: id, name: `${title}`
                       }));
                       console.log(choice);
                       return choice;
@@ -212,3 +216,217 @@ function createDepartment() {
     
         });
       };
+
+//function that will display all employees
+function viewAllEmployees() {
+
+    var query =  `SELECT employee.id, employee.first_name, employee.last_name, role.title, departments.name AS department, role.salary
+                    FROM employee 
+                    LEFT JOIN role 
+                    ON employee.role_id = role.id
+                    LEFT JOIN departments 
+                    ON departments.id = role.department_id`
+
+        connection.query(query, function (err, results) {
+            if (err) throw err;
+
+            console.table(results);
+
+            start();
+        });
+};
+
+//function that will display employees by department
+function viewByDepartment() {
+
+    connection.query(`SELECT * FROM DEPARTMENTS`, function (err, results) {
+
+        inquirer
+        .prompt([
+  
+          {
+              name: "departmentView",
+              type: "list",
+              message: "Which department do you want to view?",
+              choices: function() {
+            
+                  var choice = results.map(({ id, name }) => ({
+                      value: id, name: `${name}`
+                    }));
+                    console.log(choice);
+                    return choice;
+                },
+            },
+        ])
+
+
+    .then(answers => {
+        
+        var query =  `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary
+                    FROM departments 
+                    LEFT JOIN role 
+                    ON departments.id = role.department_id
+                    LEFT JOIN employee 
+                    ON role.id = employee.role_id
+                    WHERE departments.id = ?`
+
+        connection.query(query, answers.departmentView, function (err, results) {
+            if (err) throw err;
+
+            console.table(results);
+
+           start();
+        });
+
+    });
+
+});
+};
+
+//function that will display employees by role
+function viewByRole() {
+
+    connection.query(`SELECT * FROM ROLE`, function (err, results) {
+
+        inquirer
+        .prompt([
+  
+          {
+              name: "roleView",
+              type: "list",
+              message: "Which role do you want to view?",
+              choices: function() {
+            
+                  var choice = results.map(({ id, title }) => ({
+                      value: id, name: `${title}`
+                    }));
+                    console.log(choice);
+                    return choice;
+                },
+            },
+        ])
+
+
+    .then(answers => {
+        console.log(answers)
+        var query =  `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary
+                    FROM role 
+                    LEFT JOIN employee 
+                    ON role.id = employee.role_id
+                    WHERE role.id = ?`//<= wrong
+
+        connection.query(query, answers.roleView, function (err, results) {
+            if (err) throw err;
+
+            console.table(results);
+
+           start();
+        });
+
+    });
+
+});
+};
+
+//function that will create departments
+function createDepartment() {
+   
+    inquirer
+      .prompt([
+        {
+          name: "department",
+          type: "input",
+          message: "What is the departments name?"
+        },
+
+      ])
+      .then(function(answer) {
+      
+        connection.query(
+          "INSERT INTO departments SET ?",
+          {
+            name: answer.department
+          },
+          function(err) {
+            if (err) throw err;
+            console.log("department added!");
+       
+            start();
+          });
+      });
+  };
+
+  //function that will update roles of employees
+
+  function updateEmployeeRole() {
+    getEmployee()
+
+  function getEmployee() {
+
+    //function to get the choices array for employee
+    connection.query("SELECT * FROM employee", function(err, results) {
+        if (err) throw err;
+              
+                var employeeChoice = results.map(({ id, first_name, last_name }) => ({
+                    value: id, name: `${first_name} ${last_name}`
+                  }));
+                  console.log(employeeChoice);
+                  getRole(employeeChoice);
+              });
+            };
+
+    function getRole(employeeChoice) {
+
+    //function getting the roles array for the different roles
+    connection.query("SELECT * FROM role", function(err, results) {
+        if (err) throw err;
+                          
+                var roleChoice = results.map(({ id, title }) => ({
+                    value: id, name: `${title}`
+                }));
+                    console.log(roleChoice);
+                    updateRole(employeeChoice, roleChoice);
+             });
+        };
+
+          
+        function updateRole(employeeChoice, roleChoice) {
+
+            inquirer
+      .prompt([
+        {
+          name: "employee",
+          type: "list",
+          message: "Which employee are you updating?",
+          choices: employeeChoice
+        },
+
+        {
+            name: "role",
+            type: "list",
+            message: "What is this employees new role?",
+            choices: roleChoice
+          },
+
+      ])
+
+      .then(answers => {
+        console.log(answers.employee)
+        console.log(answers.role)
+
+        connection.query(`UPDATE employee SET role_id = ${answers.role} WHERE id = ${answers.employee}`, function(err, results) {
+            if (err) throw err;
+        
+            console.log (results)
+            start();
+        });
+
+
+      });
+     };
+
+    };
+            
+      
+
+      
